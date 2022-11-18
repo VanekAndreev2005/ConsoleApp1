7fb5c79578d710b965e5f8bf298dbe2d29af119e
VanekAndreev2005 <anda18276@gmail.com>
2022-11-18 14:29:24 +0900

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿  using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a, b, c;
+
+            Console.WriteLine("Введите первое число");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            b = Convert.ToInt32(Console.ReadLine());
+            c = a + b;
+            Console.WriteLine("Результат с = " + c);
+
+            float d;
+
+            Console.WriteLine("Введите первое число");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            b = Convert.ToInt32(Console.ReadLine());
+            d = a * b;
+            Console.WriteLine("Результат d = " +d);
+
+        }
+        private void C(float c)
+        {
+            Console.WriteLine(+c);
+        }
+
+        private void d(float d)
+        {
+            Console.WriteLine(+d);
+           
+        }
+    }
+}
